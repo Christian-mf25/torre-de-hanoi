c84369ccf02333319cf6d9f2e3c77ea517294377
Christian <christian.mf22@gmail.com>
2021-08-02 16:14:48 -0300

    Função criar discos

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function criaJogo(){
+const criaJogo = () =>{
 
-    let discos = document.getElementsByClassName('torre1')
-    let criaDisco = document.createElement('div')
+    let t1 = document.getElementById("t1")
+   
+    for(let i = 1; i <= 4; i++){
+        let disco = i
+        let novoDisco = document.createElement("div") 
+        t1.appendChild(novoDisco)
 
-    // document.appendChild(discos)
-    discos.innerText = 'teste'
+        disco === 1 ? novoDisco.classList.add("d1") : (disco === 2 ? novoDisco.classList.add("d2") : (disco === 3 ? novoDisco.classList.add("d3") : novoDisco.classList.add("d4")))
+    }
     
-}criaJogo()
+}
+criaJogo()
