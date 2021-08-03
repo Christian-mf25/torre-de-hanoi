a02425375f5064ca33817d9c7ffeccbf96a471ea
Christian <christian.mf22@gmail.com>
2021-08-02 21:24:27 -0300

    Inicio JavaScripit

--- script.js
+++ script.js
@@ -1,14 +1,153 @@
-const criaJogo = () =>{
+let t1 = document.getElementById("t1")
+let t2 = document.getElementById("t2")
+let t3 = document.getElementById("t3")
 
-    let t1 = document.getElementById("t1")
-   
-    for(let i = 1; i <= 4; i++){
-        let disco = i
-        let novoDisco = document.createElement("div") 
-        t1.appendChild(novoDisco)
 
-        disco === 1 ? novoDisco.classList.add("d1") : (disco === 2 ? novoDisco.classList.add("d2") : (disco === 3 ? novoDisco.classList.add("d3") : novoDisco.classList.add("d4")))
+// armazenar a largura do disco 
+// condição para se o disco armazenado é menor que o disco da torre
+// se for menor adiciona na torre se for maior não alterar o valor 
+
+
+let disco
+let larguraDisco 
+let larguraT1 = 0
+let larguraT2 = 0
+let larguraT3 = 0
+
+// if(t1.firstElementChild === null){
+//     larguraT1 = 1
+// }else{
+//     larguraT1 = t1.firstElementChild.clientWidth
+// }
+
+
+// if(t2.firstElementChild === null){
+//     larguraT2 = 2
+// }else{
+//     larguraT2 = t2.firstElementChild.clientWidth
+// }
+
+
+// if(t3.firstElementChild === null){
+//     larguraT3 = 3
+// }else{
+//     larguraT3 = t3.firstElementChild.clientWidth
+// }
+
+// const valorDisco = (primeiro, torre) =>{
+
+    
+//     if(disco === undefined){
+//         primeiro.remove()
+//         disco = primeiro
+//     }else{
+//         torre.appendChild(disco)
+//         disco = undefined
+//         console.log(larguraT1, larguraT2, larguraT3)
+//     }
+
+// }
+
+t1.addEventListener("click", function(){
+        
+    let primeiro = t1.firstElementChild
+    let largura = primeiro.clientWidth
+    let torre = t1
+
+
+    if(primeiro === null){
+        larguraT1 = 1
+    }else{
+        larguraT1 = primeiro.clientWidth
+    }
+
+
+
+    // valorDisco(primeiro, torre)
+    
+    if(disco === undefined){
+        larguraDisco = largura
+        primeiro.remove()
+        disco = primeiro
+        
+
+    }else{
+        t1.appendChild(disco)
+        disco = undefined
+
+
+        if(primeiro !== null){
+            larguraT1 = primeiro.clientWidth
+        }
+        console.log(larguraT1, larguraT2, larguraT3)
     }
     
-}
-criaJogo()
+    
+
+})
+
+t2.addEventListener("click", function(){
+
+    let primeiro = t2.firstElementChild
+    let torre = t2
+
+    // valorDisco(primeiro, torre)
+
+
+    if(primeiro !== null){
+        larguraT2 = primeiro.clientWidth
+        console.log(larguraT2 )
+    }
+
+
+
+
+    if(disco === undefined){
+        primeiro.remove()
+        disco = primeiro
+    }else{
+        t2.appendChild(disco)
+        disco = undefined
+
+
+ 
+    }
+
+
+
+
+})
+
+t3.addEventListener("click", function(){
+
+
+    let primeiro = t3.firstElementChild
+    let torre = t3
+
+    // valorDisco(primeiro, torre)
+
+
+    if(primeiro !== null){
+        larguraT3 = primeiro.clientWidth
+        console.log(larguraT3)
+    }
+
+
+
+
+    if(disco === undefined){
+        primeiro.remove()
+        disco = primeiro
+    }else{
+        t3.appendChild(disco)
+        disco = undefined
+
+
+        
+    }
+
+    
+})
+
+
+// console.log(larguraT1, larguraT2, larguraT3)
